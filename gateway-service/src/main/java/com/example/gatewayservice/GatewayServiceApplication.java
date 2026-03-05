36adf8b207b9d154d2666a2a476c663676193c5f
BensaltanaHoussam <bensaltanahoussam@gmail.com>
2026-03-05 14:53:51 +0000

    STREAM-20 & STREAM-22: Enable Discovery Client, update POM to Spring Boot 3.2.3 and Spring Cloud Gateway

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,19 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Spring Cloud API Gateway.
+ * Single entry point for routing requests to microservices.
+ *
+ * STREAM-22: Enable Discovery Client for Eureka integration
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
 }
